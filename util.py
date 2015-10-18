be53fadea058154f531a0aa5664512dbfa702099
Sean Chu <chussean@gmail.com>
2015-10-18 17:02:35 -0400

    Added Titles to table

--- util.py
+++ util.py
@@ -3,20 +3,20 @@ from os import listdir
 from os.path import isfile, join
 import md5
 
-def add(filename, username, content):
+def add(filename, username, title, content):
         if not isfile(join('tables/',filename)):
                 file = open('tables/' + filename, 'w+')
                 new = sqlite3.connect('tables/' + filename)
                 c = new.cursor()
-                q = "CREATE TABLE content (user text, content text)"
+                q = "CREATE TABLE content (user text, title text, content text)"
                 c.execute(q)
                 new.commit()
                 file.close()
         conn = sqlite3.connect('tables/' + filename)
         c = conn.cursor()
 
-        TEMPLATE="INSERT INTO content VALUES ('%(user)s','%(content)s')"
-        q = TEMPLATE%({'user':username,'content':content})
+        TEMPLATE="INSERT INTO content VALUES ('%(user)s','%(title)s', %(content)s')"
+        q = TEMPLATE%({'user':username,'title':title, content':content})
         c.execute(q)
         conn.commit()
 
@@ -51,8 +51,8 @@ def register(uname,pword):
     f.close()
     return True
 
-add("hi.db","user","content")
-add("hi.db","leon","hello")
+add("hi.db","user", "title","content")
+add("hi.db","leon", "Something", "hello")
 
 def authenticate(uname, pword):
 	if uname =="Sir" and pword == "Loin":
