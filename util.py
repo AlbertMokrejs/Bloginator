0c241c3c77394948cc9aefcba31f11e7535792bb
Albert Mokrejs <albertmokrejs@gmail.com>
2015-10-23 13:05:04 -0400

    Update util.py

--- util.py
+++ util.py
@@ -4,28 +4,25 @@ from os.path import isfile, join
 import md5
 
 def add(filename, username, content):
-        if not isfile(join('tables/',filename)):
-                file = open('tables/' + filename, 'w+')
-                new = sqlite3.connect('tables/' + filename)
-                c = new.cursor()
-                q = "CREATE TABLE content (user text, content text)"
-                c.execute(q)
-                new.commit()
-                file.close()
-        conn = sqlite3.connect('tables/' + filename)
-        c = conn.cursor()
-
-        TEMPLATE="INSERT INTO content VALUES ('%(user)s', '%(content)s')"
-        q = TEMPLATE%({'user':username, 'content':content})
-        c.execute(q)
-        conn.commit()
-
+#        if not isfile(join('tables/',filename)):
+ #               file = open('tables/' + filename, 'w+')
+  #              new = sqlite3.connect('tables/' + filename)
+   #             c = new.cursor()
+    #            q = "CREATE TABLE content (user text, content text)"
+     #           c.execute(q)
+      #          new.commit()
+       #         file.close()
+        conn = MongoClient()
+        c = conn[filename]
+        q = {'user':username, 'content':content}
+        c.main.insert(q)
+        
 def getTables():
         onlyfiles=[f for f in listdir('tables/') if isfile(join('tables/',f))]
         return onlyfiles
 
-
 def authenticate(uname, pword):
+        ##should be fine
     m = md5.new()
     m.update(pword)
     f = open("tables/users.txt",'r')
@@ -37,6 +34,7 @@ def authenticate(uname, pword):
     return False
 
 def register(uname,pword):
+        ##should be fine. These ****s used encryption, I think. It makes the code so gross.
     m=md5.new()
     m.update(pword)
     f = open("tables/users.txt", 'r')
