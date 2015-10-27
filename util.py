5d33f318d90e8d29f20d46062b2930be3c02ed43
Albert Mokrejs <albertmokrejs@gmail.com>
2015-10-26 22:15:12 -0400

    Update util.py
    added some further code analysis. Fixed one likely error.

--- util.py
+++ util.py
@@ -1,4 +1,4 @@
-import sqlite3
+# import sqlite3
 import random
 from pymongo import MongoClient
 from os import listdir
@@ -15,11 +15,13 @@ def add(filename, username, content):
       #          new.commit()
        #         file.close()
 #Mongo stores files in a seperate place, the above code is likely useless
+# !A!
         conn = MongoClient()
         c = conn["main"]
         q = {'user':username, 'content':content}
         c[filename].insert(q)
 #not really sure what this method is for, might work though.
+#it requires not making a new file, but instead makes a new table in. 
         
 #def getTables():
  #Likely useless as the files are stored elsewhere.... need a rewrite
@@ -27,31 +29,36 @@ def add(filename, username, content):
        # return onlyfiles
 
 # WE NEED TO MAKE A FUNCTION THAT GETS THE TABLES FROM WHEREVER MONGO SAVES IT
+# SEE !A!, it doesn't make multiple files now
  
 
 def authenticate(uname, pword):
         ##should be fine
     m = md5.new()
     m.update(pword)
+    #hashes password
     f = open("tables/users.txt",'r')
     for line in f.readlines():
         if uname == line.split(',')[0] and m.hexdigest() == line.split(',')[1].strip():
+         #compares hashed password to recorded password
             f.close()
+            #ends loop, closes file
             return True
     f.close()
     return False
- #PWords/Unames are stored in a text file which is annoying / gross
+ #PWords/Unames are stored in a text file which is fine and should hopefully work. 
 
 def register(uname,pword):
         ##should be fine. They used encryption, I think. It makes the code so gross.
         ##wanna get rid of encryption?
-        ##yes, maybe
+        ##No, it's already implemented. We can work around it.
     m=md5.new()
     m.update(pword)
     f = open("tables/users.txt", 'r')
     for line in f.readlines():
         if uname == line.split(',')[0]:
             return False
+            #prevents duplicated
     f.close()
     f = open("tables/users.txt",'a')
     #f.write("%s,%s\n"%(uname,m.hexdigest()))
@@ -59,13 +66,14 @@ def register(uname,pword):
     f.write("%(user)s,%(phash)s\n"%({"user":uname,"phash":m.hexdigest()}))
     f.close()
     return True
+    #returns true if succesful. A false is an error, probably declared on the page. 
 
 def getposts(title):
         conn = MongoClient()
         c = conn["main"]
-        info = c[filename].find({"user": true, "content":true, "_id":false})
+        info = c[title].find({"user": true, "content":true, "_id":false})
         return info
-        #might be broken, no idea. No idea what the heck all these things do.
+        #might be broken, no idea. No idea what the heck all these things do. Assuming it is supposed to find a specific post, it might work with !A!.
 
 def gettitles():
     titles = []
