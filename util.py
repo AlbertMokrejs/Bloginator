3ec62a882d69b0bb409cb69a77a4ce33860dd1d4
Albert Mokrejs <albertmokrejs@gmail.com>
2015-10-28 13:19:04 -0400

    Update util.py

--- util.py
+++ util.py
@@ -85,7 +85,8 @@ def gettitles():
     titles = []
     titless = c.collection_names(False)
     for f in titless:
-        titles.append(f[:-3])
+        titles.append(f)
+        #[:-3]
     return titles
     #This is 100% BROKEN, as our files are stored somewhere else. We'll need to replace this.
     #where the hell does Mongo save files?? I dont get it
